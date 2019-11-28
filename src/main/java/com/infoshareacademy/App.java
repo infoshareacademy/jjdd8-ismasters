2c3ee7e42ac66a7fcfc84cd0c9f78c3cd7924845
Chris <ichris.kk@gmail.com>
2019-11-28 16:42:52 +0100

    Updated maven project structure

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,11 +1,6 @@
 package com.infoshareacademy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
