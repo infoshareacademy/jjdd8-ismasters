13efa198d7f55be4b0b5fd967f17a22f7d31feee
ChrisMcCat <ichris.kk@gmail.com>
2019-11-28 19:58:13 +0100

    cutted out test path with junit and added logger so we can be smashed

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,8 +1,13 @@
 package com.infoshareacademy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+
+    public static void main( String[] args ) {
+        stdout.info( "Hello World!" );
     }
 }
