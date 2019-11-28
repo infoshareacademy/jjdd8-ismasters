0cbfc879615546a467ec0119604eb14455585f4b
ChrisMcCat <ichris.kk@gmail.com>
2019-11-28 20:44:29 +0100

    white space 2

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class App {
 
-    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void main(String[] args) {
         stdout.info("Hello World!");
