36369466de91cf7662b98622928aebec4e51410e
ChrisMcCat <ichris.kk@gmail.com>
2019-12-09 15:13:48 +0100

    sonar fixes

--- src/main/java/com/infoshareacademy/menu/HeaderPrinter.java
+++ src/main/java/com/infoshareacademy/menu/HeaderPrinter.java
@@ -6,22 +6,24 @@ import org.slf4j.LoggerFactory;
 public class HeaderPrinter {
 
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+    private static final String ramka1 = "│                                                    │\n";
+    private static final String ramka2 = "│                                                                  │\n";
 
     void printHeader() {
         stdout.info("┌────────────────────────────────────────────────────┐\n");
-        stdout.info("│                                                    │\n");
+        stdout.info(ramka1);
         stdout.info("│       " + ColorHandler.YELLOW + "Wybierz pozycję z menu używając cyfr       " + ColorHandler.DEFAULT + "  │\n");
         stdout.info("│                " + ColorHandler.YELLOW + "  na klawiaturze    " + ColorHandler.DEFAULT + "                │\n");
-        stdout.info("│                                                    │\n");
+        stdout.info(ramka1);
     }
 
     void printBiggerHeader() {
         stdout.info("┌──────────────────────────────────────────────────────────────────┐\n");
-        stdout.info("│                                                                  │\n");
+        stdout.info(ramka2);
         stdout.info("│       " + ColorHandler.YELLOW + "       Wybierz pozycję z menu używając cyfr               " + ColorHandler.DEFAULT + " │\n");
         stdout.info("│                " + ColorHandler.YELLOW + "        na klawiaturze    " + ColorHandler.DEFAULT + "                        │\n");
-        stdout.info("│                                                                  │\n");
-        stdout.info("│                                                                  │\n");
+        stdout.info(ramka2);
+        stdout.info(ramka2);
     }
 
 }
