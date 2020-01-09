138b3bbf34e030949cb4c5bbb065e72a6bbe21f5
Grzegorz Chyla <grzegorz.chyla@gmail.com>
2020-01-09 22:48:09 +0100

    Add entities

---
+++ web/src/main/java/com/isa/domain/entity/TicketType.java
@@ -0,0 +1,48 @@
+package com.isa.domain.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "ticket_type")
+public class TicketType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "ticket_type_name")
+    private String ticketTypeName;
+
+    @OneToMany (mappedBy = "ticket_type")
+    private List<TicketType> ticketType;
+
+    public TicketType(String ticketTypeName) {
+        this.ticketTypeName = ticketTypeName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTicketTypeName() {
+        return ticketTypeName;
+    }
+
+    public void setTicketTypeName(String ticketTypeName) {
+        this.ticketTypeName = ticketTypeName;
+    }
+
+    public List<TicketType> getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(List<TicketType> ticketType) {
+        this.ticketType = ticketType;
+    }
+}
---
+++ web/src/main/java/com/isa/domain/entity/Url.java
@@ -0,0 +1,69 @@
+package com.isa.domain.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "www_url")
+    private String wwwUrl;
+
+    @Column (name = "fb_url")
+    private String fbUrl;
+
+    @Column (name = "fb_url")
+    private String tickerUrl;
+
+    @OneToOne (mappedBy = "url")
+    private Event event;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWwwUrl() {
+        return wwwUrl;
+    }
+
+    public void setWwwUrl(String wwwUrl) {
+        this.wwwUrl = wwwUrl;
+    }
+
+    public String getFbUrl() {
+        return fbUrl;
+    }
+
+    public void setFbUrl(String fbUrl) {
+        this.fbUrl = fbUrl;
+    }
+
+    public String getTickerUrl() {
+        return tickerUrl;
+    }
+
+    public void setTickerUrl(String tickerUrl) {
+        this.tickerUrl = tickerUrl;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Url(String wwwUrl) {
+        this.wwwUrl = wwwUrl;
+    }
+}
