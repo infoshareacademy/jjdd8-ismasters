6e28691380fc003fdfb8c8fc4d989beb87817682
ChrisMcCat <ichris.kk@gmail.com>
2020-01-17 17:34:37 +0100

    refactored

--- web/src/main/java/com/isa/servlet/SingleEventServlet.java
+++ web/src/main/java/com/isa/servlet/SingleEventServlet.java
@@ -29,14 +29,6 @@ public class SingleEventServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=UTF-8");
 
-//        String event = req.getParameter("event");
-//        if (event == null || event.isEmpty()) {
-//            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//            logger.error("Bad request");
-//            logger.debug("Bad request");
-//            return;
-//        }
-
         Template template = templateProvider.getTemplate(getServletContext(),"singleEvent-section.ftlh");
 
         Object model = eventService.getSingleEvent();
