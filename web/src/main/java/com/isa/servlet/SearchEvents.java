eddfee09d34cfb8a845db2a207f0d8e2532fd6d8
ChrisMcCat <ichris.kk@gmail.com>
2020-01-20 00:04:30 +0100

    small tweaks into searchservlet

--- web/src/main/java/com/isa/servlet/SearchEvents.java
+++ web/src/main/java/com/isa/servlet/SearchEvents.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,7 +40,9 @@ public class SearchEvents extends HttpServlet {
             return;
         }
 
-        List<EventDto> foundEvents = searchService.searchEvents(search);
+        List<EventDto> foundEvents = new ArrayList<>();
+
+        foundEvents.addAll(searchService.searchEvents(search));
 
         Template template = templateProvider.getTemplate(getServletContext(), "search.ftlh");
         Map<String, Object> model = new HashMap<>();
