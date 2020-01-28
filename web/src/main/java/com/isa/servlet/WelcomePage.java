20d55b6411633f8456ea5d52192eae71027f45d3
Grzegorz Chyla <grzegorz.chyla@gmail.com>
2020-01-28 16:49:31 +0100

    Working Oauth2

--- web/src/main/java/com/isa/servlet/WelcomePage.java
+++ web/src/main/java/com/isa/servlet/WelcomePage.java
@@ -11,11 +11,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,12 +26,8 @@ import java.util.Map;
 @WebServlet ("")
 public class WelcomePage extends HttpServlet {
 
-
     public static final String FILENAME = "Json_example.json";
-
-
     private final Logger logger = LoggerFactory.getLogger(getClass().getName());
-
     private EventDTO_mock eventDTO_mock = new EventDTO_mock();
 
     @Inject
@@ -36,74 +35,53 @@ public class WelcomePage extends HttpServlet {
 
     @Inject
     private TemplateProvider templateProvider;
+    private GoogleAuthHelper googleAuthHelper = new GoogleAuthHelper();
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws SecurityException, IOException {
         eventDTO_mock.setDescShort("Gimnastyka prozdrowotna dla kobiet 50 +Zaj\u0119cia prozdrowotne i uelastyczniaj\u0105ce dla kobiet 50+.Grupa wiekowa: Kobiety 50+Cena: 20 z\u0142 \u2013 pojedyncze zaj\u0119cia,70 z\u0142 - miesi\u0105c (1x w tygodniu)120 z\u0142 - miesi\u0105c (2x w tygodniu)Cz\u0119stotliwo\u015b\u0107 zaj\u0119\u0107...");
         eventDTO_mock.setName("Gimnastyka prozdrowotna");
         eventDTO_mock.setUrls("http://wyspaskarbow.gak.gda.pl");
-        Boolean loginStatus;
 
-        final GoogleAuthHelper helper = new GoogleAuthHelper();
         Template template = templateProvider.getTemplate(getServletContext(), "welcome-page.ftlh");
         Map<String, Object> model = new HashMap<>();
-        String sessionAttribute = (String) req.getSession().getAttribute("state");
 
         model.put("eventDTO_mock", eventDTO_mock);
-//        model.put("userName", req.getSession().getAttribute("userName"));
-//        logger.info("UserName: {}", req.getSession().getAttribute("userName"));
-//        logger.info("Session Attributes: {}", req.getSession().getAttributeNames().toString());
 
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("Content-Type = text/hml");
+        resp.setContentType("Content-Type = text/html");
 
-        if (req.getParameter("code") == null || req.getParameter("state") == null) {
-            logger.info("No user logged");
+        String code = req.getParameter("code");
+        logger.info("Code initial {}", code);
+        String state = req.getParameter("state");
+        logger.info("State initial {}", state);
 
+        if ( (code != null) && !code.isEmpty() && (state != null) && !state.isEmpty()) {
 
-//            writer.println("<a href='" + helper.buildLoginUrl() + "'>log in with google</a>");
-            String token = helper.getStateToken();
-            req.getSession().setAttribute("state", token);
-            logger.info("Token: {}", token);
+            if (req.getSession().getAttribute("googleId") == null) {
+                String googleJson = googleAuthHelper.getUserInfoJson(code);
+                JsonObject googleUser = Json.createReader(new StringReader(googleJson)).readObject();
 
-            sessionAttribute = (String) req.getSession().getAttribute("state");
-            model.put("loginUrl", helper.buildLoginUrl());
-            model.put("token", token);
-            model.put("code", req.getParameter("code"));
-            model.put("sessionAttribute", sessionAttribute);
-
-            loginStatus = false;
-            model.put("loginStatus", loginStatus);
-
-            try {
-                template.process(model, resp.getWriter());
-            } catch (TemplateException e) {
-                logger.error(e.getMessage());
+                logger.info("Google user logged {}", googleUser.getString("email"));
+                req.getSession().setAttribute("googleId", googleUser.getString("id"));
             }
+        }
+            final String googleId = (String) req.getSession().getAttribute("googleId");
+            logger.info("GoogleId: {}", googleId);
 
-        } else if (req.getParameter("code") != null && req.getParameter("state") != null
-                && req.getParameter("state").equals(sessionAttribute)) {
+        if (googleId != null && !googleId.isEmpty()) {
+            model.put("logged", "yes");
+        } else {
+            model.put("logged", "no");
+            model.put("loginUrl", googleAuthHelper.buildLoginUrl());
+        }
 
-            req.getSession().setAttribute("admin", true);
-            sessionAttribute = (String) req.getSession().getAttribute("state");
-
-            logger.info("User logged");
-
-//            model.put("loginInfo", helper.getUserInfoJson(req.getParameter("state")));
-            model.put("state", req.getParameter("state"));
-            model.put("code", req.getParameter("code"));
-            model.put("sessionAttribute", sessionAttribute);
-
-            loginStatus = true;
-            model.put("loginStatus", loginStatus);
-
-            try {
-                template.process(model, resp.getWriter());
-            } catch (TemplateException e) {
-                logger.error(e.getMessage());
-            }
-
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            logger.error(e.getMessage());
         }
     }
 }
