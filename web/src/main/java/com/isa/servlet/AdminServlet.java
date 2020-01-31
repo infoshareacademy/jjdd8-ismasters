abf76ace683f654a5b18da79ec7eaf3da86b891e
Robert <Robert.hilla@gmail.com>
2020-01-31 21:52:44 +0100

    all updated

--- web/src/main/java/com/isa/servlet/AdminServlet.java
+++ web/src/main/java/com/isa/servlet/AdminServlet.java
@@ -1,7 +1,50 @@
 package com.isa.servlet;
 
 
+import com.isa.config.TemplateProvider;
+import com.isa.domain.dto.EventDto;
+import com.isa.service.domain.EventService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/admin-template")
+public class AdminServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Inject
+    private EventService eventService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws SecurityException, IOException {
 
 
-public class AdminServlet {
+
+        Template template = templateProvider.getTemplate(getServletContext(), "admin-view.ftlh");
+        Map<String, Object> model = new HashMap<>();
+
+
+
+        try {
+            template.process(model, rep.getWriter());
+        } catch (TemplateException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
 }
