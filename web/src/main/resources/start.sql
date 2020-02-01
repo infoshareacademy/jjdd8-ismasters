4736aed6286287c4259c7c7977af864b1cfb050a
Grzegorz Chyla <grzegorz.chyla@gmail.com>
2020-02-01 18:08:08 +0100

    Add start.sql

--- web/src/main/resources/start.sql
+++ web/src/main/resources/start.sql
@@ -1,4 +1,6 @@
 USE 3city_events;
 
 insert into user(email, google_id, name, user_type) values ('ismasters.infoshare@gmail.com', '111592914360020898028', 'isMasters Admin', 'SUPER_ADMIN');
+insert into user(email, google_id, name, user_type) values ('xx@xx.com', '65643445745', 'fakeUser1', 'USER');
+insert into user(email, google_id, name, user_type) values ('abc@abc.com', '9565764262457', 'fakeUser2', 'USER');
 
