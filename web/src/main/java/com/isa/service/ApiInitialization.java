9a5c7c3a0dfadbea5ab6dbb226d9a2d81bc0dba5
Robert <Robert.hilla@gmail.com>
2020-02-02 17:41:41 +0100

    update with new dependency

--- web/src/main/java/com/isa/service/ApiInitialization.java
+++ web/src/main/java/com/isa/service/ApiInitialization.java
@@ -33,13 +33,14 @@ public class ApiInitialization {
     private PlaceService placeService;
 
 
+    @Inject
+    private MailService mailService;
     @PostConstruct
     public void apiInitialization() throws IOException {
 
         organizersService.setRelations(converter.buildClientRequest(constantValuesBean.getOrganizersApi()));
         placeService.setRelations(converter.buildClientRequest(constantValuesBean.getPlaceApi()));
         eventService.mapApiToEntity(converter.buildClientRequest(constantValuesBean.getEventsApi()));
-
 
 
     }
